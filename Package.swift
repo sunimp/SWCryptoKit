f8a5cc8882a6e0c5c431074130c338d94df00460
杨光 <yangguang@webull.com>
2024-08-28 10:11:06 +0800

    fix: typo

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/sunimp/WWExtensions.Swift.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/sunimp/WWExtensions.Swift.git", .upToNextMajor(from: "1.1.1")),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/sunimp/secp256k1-swift.git", .upToNextMajor(from: "0.17.2")),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.54.0"),
